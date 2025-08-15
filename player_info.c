6dd06d5c03e6b77b868a222060c19f43a3fc737f
Zhiyong Wong <joshua.zyw@gmail.com>
2025-08-15 22:21:17 +1000

    now can update_player_pos, acceleration, and deceleration

--- player_info.c
+++ player_info.c
@@ -6,42 +6,58 @@
 #define MIN_SPEED 1
 #define PI 3.14
 
-int stdin_angle = 0; //placeholder, will replace with actual angle given by stdin
-double stdin_distance = 0;
+int fazell_angle = 0; //placeholder, will replace with actual angle given by stdin
+int fazell_pos_x = 0;
+int fazell_pos_y = 0;
+int fazell_speed = 1;
 
 typedef struct{
-    int postion_x;
+    int position_x;
     int position_y; 
     int angle_degrees;
     int speed;
 } Player_info;
 
-Player_info* init_player_info(){
+Player_info* init_player_info(int fazell_angle, int fazell_pos_x, int fazell_pos_y, int fazell_speed){
+    //this just takes all of fazell's values, and updates the struct
     Player_info* P_info = malloc(sizeof(Player_info));
-    P_info->position_y = 0;
-    P_info->postion_x = 0;
-    P_info->angle_degrees = 0;
-    P_info->speed = 0;
+    P_info->position_y = fazell_pos_y;
+    P_info->position_x = fazell_pos_x;
+    P_info->angle_degrees = fazell_angle;
+    P_info->speed = fazell_speed;
 
     return P_info;
 }
 
-void move_player_direction(Player_info* P_info, int stdin_angle, double stdin_distance){
-    P_info->angle_degrees = stdin_angle;
+void update_player_pos(Player_info* P_info){
     double angle_radians = ((P_info->angle_degrees) * PI) / 180;
 
-    double delta_x = stdin_distance*cos(angle_radians);
-    double delta_y = stdin_distance*sin(angle_radians);
+    //calculates movement of x and y combined, with mouse movement(cos/sin)
+    double delta_x = (P_info->position_x)*cos(angle_radians);
+    double delta_y = (P_info->position_y)*sin(angle_radians);
 
-    P_info->postion_x += delta_x;
+    //updates player position
+    P_info->position_x += delta_x;
     P_info->position_y += delta_y;
-    
-
 
 }
 
+void acceleration(Player_info* P_info){
+    
+    for(int i = P_info->speed; i<MAX_SPEED; i++){
+        P_info->speed += 10; //increases by 10 each iteration it is not at max speed
+    }
+    
+}
+
+void deceleration(Player_info* P_info){
+    for (int i = P_info->speed; i>MIN_SPEED; i--){
+        P_info->speed -= 10;
+    } 
+}
+
 int main(void){
-    Player_info* P_info = init_player_info();
-    move_player_direction(P_info, stdin_angle, stdin_distance);
+    Player_info* P_info = init_player_info(fazell_angle, fazell_pos_x, fazell_pos_y, fazell_speed);
+    update_player_pos(P_info);
 
 }
