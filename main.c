9476eaf1dbfb03a23c6f72b9899a0d0d4a08ec91
jjjeidennn <jeiden@jeiden.localdomain>
2025-08-16 00:44:05 +1000

    adjust accel/decel increm/decrem values, modified update_player_pos delta_x/y calc in player_info.c . modified render_screen to take player as arg

--- main.c
+++ main.c
@@ -1,27 +1,55 @@
 #include <ncurses.h>
+#include "player_info.h"
 #include "render.h"
 
 // add rendering stuff here
-void render_screen() {
+void render_screen(Player_info* player) {
     clear_screen();
 
-    // do the actual rendering
-    render_line(5, 10, 10, '@');
-    render_line(15, 15, 5, '@');
-    render_line(100, 30, 30, '@');
+    // Player_info* player = init_player_info(5, 5, 0, 1);
 
-    render_rect(80, 5, 50, 12, '#');
+    // do the actual rendering
+    //render_line(5, 10, 10, '@');
+    //render_line(15, 15, 5, '@');
+    //render_line(100, 30, 30, '@');
+
+    render_rect(player->position_x, player->position_y, 4, 2, '#');
 
     refresh();
 }
 
 int main() {
+    Player_info* player = init_player_info(0, 0, 0, 1);
+
     // setup the screen
     setup_screen();
 
-    render_screen();
+    for(int i = 0; i < 10; i++) {  // move 10 steps
+        clear_screen();
 
-    getch(); // stall until a key has been pressed
+        update_player_pos(player);   // move player forward
+        acceleration(player);
+        render_screen(player);
+        update_player_pos(player);   // move player forward
+
+        refresh();
+        usleep(100000);  // 0.1s per frame
+    }
+
+    for(int i = 0; i < 30; i++) {  // move 10 steps
+        clear_screen();
+
+        update_player_pos(player);   // move player forward
+        deceleration(player);
+        render_screen(player);
+        update_player_pos(player);   // move player forward
+
+
+        refresh();
+        usleep(100000);  // 0.1s per frame
+    }
+    
+    free(player);
     endwin();
     return 0;
 }
---
+++ player_info.h
@@ -0,0 +1,28 @@
+#ifndef PLAYER_INFO_H
+#define PLAYER_INFO_H
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include "render.h"
+
+#define MAX_SPEED 100
+#define MIN_SPEED 1
+#define PI 3.14
+
+typedef struct{
+    int position_x;
+    int position_y; 
+    int angle_degrees;
+    int speed;
+} Player_info;
+
+Player_info* init_player_info(int fazell_angle, int fazell_pos_x, int fazell_pos_y, int fazell_speed);
+
+void update_player_pos(Player_info* P_info);
+
+void acceleration(Player_info* P_info);
+
+void deceleration(Player_info* P_info);
+
+#endif
