faaeb65c8a081c6a1d81ff11ee29bb14861db66d
Kartik-Br <s4883101@student.uq.edu.au>
2025-08-16 00:34:35 +1000

    successfull udp connection. it just sents "bumass client" back to the client.

--- serverClient/wolfserver.c
+++ serverClient/wolfserver.c
@@ -23,99 +23,63 @@ typedef struct {
     int maxPlayers;
 } gameState;
 
-
-int listen_on_port(char* port){
-    struct addrinfo* ai = 0;
-    struct addrinfo hints;
-    memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_family = AF_INET; // IPV4
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE; // listen on all IP addresses`
-    int err;
-    if ((err = getaddrinfo(NULL, port, &hints, &ai))) {
-        freeaddrinfo(ai);
-        return -1; // Could not determine address
-    }
-
-    int listenfd = socket(AF_INET, SOCK_STREAM, 0); 
-    if (listenfd < 0) {
-        freeaddrinfo(ai);
-        return -1;
-    }
-    int optVal = 1;
-    if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &optVal, sizeof(int))
-            < 0) {
-        freeaddrinfo(ai);
-        return -1;
-    }
-    // Bind socket to address
-    if (bind(listenfd, ai->ai_addr, sizeof(struct sockaddr)) < 0) {
-
-        freeaddrinfo(ai);
-        return -1;
-    }
-    // Indicate willingness to listen on a socet - connections can now be queued
-    if (listen(listenfd, 1) < 0) { // Up to 10 connection requests can queue
-        freeaddrinfo(ai);
-        return -1;
-    }
-    struct sockaddr_in portInfo;
-    socklen_t len = sizeof(portInfo);
-    getsockname(listenfd, (struct sockaddr*)&portInfo, &len);
-    int actualPort = ntohs(portInfo.sin_port);
-    fprintf(stderr, "%d\n", actualPort);
-    fflush(stderr);
-    freeaddrinfo(ai);
-    // Have listening socket - return it
-    return listenfd;
-   }
-
-void *handle_client(void *client_socket) {
-    int clientfd = *(int *)client_socket;
+void* handle_data(int sockfd) {
+    struct sockaddr_in client_addr;
+    socklen_t addr_len = sizeof(client_addr);
     char buffer[BUFSIZ];
+    printf("gurttt");
 
-    while(1){
-	memset(buffer, 0, sizeof(buffer));
-	int bytes_recieved = recv(clientfd, buffer, sizeof(buffer), 0);
-	if(bytes_recieved <= 0) {
-	    printf("bumass client gone\n");
-	    break;
-	}
-	printf("Client: %s\n", buffer);
-	send(clientfd, "bumass client", strlen("bumass client"), 0);
+    while (1) {
+	int n = recvfrom(sockfd, buffer, BUFSIZ, 0, (struct sockaddr*)&client_addr, &addr_len);
+	if(n>0) {
+	    buffer[n] = '\0';
+	    printf("bumass client sent: %s", buffer);
+	} 
+
+	const char *msg = "deez nuts bumass client";
+	sendto(sockfd, msg, strlen(msg), 0, (struct sockaddr *)&client_addr, 
+		addr_len);
+
+
     }
-    close(clientfd);
-    free(client_socket);
+    close(sockfd);
     return NULL;
 }
 
-void connect_everything(int server) {
-    int client_fd;
-    struct sockaddr_in fromAddr;
-    socklen_t fromAddrSize;
-    while (1) {
-        fromAddrSize = sizeof(struct sockaddr_in);
-        client_fd = accept(server, (struct sockaddr*)&fromAddr, &fromAddrSize);
-        if (client_fd < 0) {
-	    perror("something went wrong number 1\n");
-	    exit(-1);
-        }
 
-        char hostname[NI_MAXHOST];
-        int error = getnameinfo((struct sockaddr*)&fromAddr, fromAddrSize,
-                hostname, NI_MAXHOST, NULL, 0, 0);
-        if (error) {
-	    perror("something went wrong\n");
-	    exit(-1);
-        }
 
-	int *client_socket = malloc(sizeof(int));
-	*client_socket = client_fd;
-	pthread_t thread_id;
-	pthread_create(&thread_id, NULL, handle_client, client_socket);
-        pthread_detach(thread_id);
+#define PORT 8080
+
+
+int listen_on_port(int port){
+    int listenfd = socket(AF_INET, SOCK_DGRAM, 0); 
+    if (listenfd < 0) {
+	perror("fd is gone to sleep");
+        exit(-1);
     }
 
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET; //IPv4
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port);
+    // Bind socket to address
+    if (bind(listenfd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+	perror("fass bind failed\n");
+	close(listenfd);
+        exit(-1);
+    }
+    printf("UDP connection on Port 8080\n");
+    handle_data(listenfd);
+    return listenfd;
+
+
+
+   }
+
+void connect_everything(int server) {
+
+        
 
 
 }
@@ -131,8 +95,9 @@ int main(){
 
     //char* port = argv[1];
     // udp: sock_dgram 
-    int server = listen_on_port("0");
+    int server = listen_on_port(PORT);
     connect_everything(server);
+    printf("gurt");
 
 }
     
