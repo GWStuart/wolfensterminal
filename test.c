5a566fe6b5e550cd25eff6c693349d462b1500f8
GWStuart <89712016+GWStuart@users.noreply.github.com>
2025-08-16 14:25:35 +1000

    example of rendering player

--- test.c
+++ test.c
@@ -2,14 +2,15 @@
 #include <unistd.h>
 #include "player_info.h"
 #include "render.h"
+#include <stdbool.h>
 
 // add rendering stuff here
 void render_screen() {
     clear_screen();
 
     // do the actual rendering
-    for (int i=0; i<32; i++) {
-        render_line_texture(5 + i, 2, 32, '@', i);
+    for (int i=0; i<14; i++) {
+        render_player(5 + i, 2, i, 50);
     }
 
     refresh();
