4c9bcfc1ef5d7541e26624a29f12a9ca99022a86
Zhiyong Wong <joshua.zyw@gmail.com>
2025-08-16 15:23:01 +1000

    I PRAY ACCELERATION WORKS

--- player_info.h
+++ player_info.h
@@ -5,15 +5,14 @@
 #include <math.h>
 #include <stdlib.h>
 #include "render.h"
+#include "input.h"
 
 #include "config.h"
 
-Player_info* init_player_info(int fazell_angle, int fazell_pos_x, int fazell_pos_y, int fazell_max_speed, int fazell_curr_speed);
+Player_info* init_player_info(int fazell_angle, int fazell_pos_x, int fazell_pos_y, int faz_curr_speed, int faz_max_speed);
 
 void update_player_pos(Player_info* P_info);
 
-void acceleration(Player_info* P_info);
-
-void deceleration(Player_info* P_info);
+void acceleration(Player_info* P_info, Inputs* inputs);
 
 #endif
