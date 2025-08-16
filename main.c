9fc1488fb60d35f1b6ac4af54f418db46384f0d8
Zhiyong Wong <joshua.zyw@gmail.com>
2025-08-16 22:38:32 +1000

    final commit for this branch

--- main.c
+++ main.c
@@ -3,53 +3,18 @@
 #include "render.h"
 #include "map_editor.h"
 
-
-// add rendering stuff here
-void render_screen(Player_info* player) {
-    clear_screen();
-
-    // Player_info* player = init_player_info(5, 5, 0, 1);
-
-    // do the actual rendering
-    //render_line(5, 10, 10, '@');
-    //render_line(15, 15, 5, '@');
-    //render_line(100, 30, 30, '@');
-
-    render_rect(player->position_x, player->position_y, 4, 2, '#');
-
-    refresh();
-}
-
 int main() {
     // setup the screen
     setup_screen();
 
-    Player_info* player = init_player_info(0, 0, 0, 1);
+    //Player_info* player = init_player_info(0, 0, 0, 1);
 
     Level* level = create_level(10, 10);
 
     map_edit(level);
 
-
-
-    for (int i = 0; i < 20; i++) {
-        clear_screen();
-
-        update_player_pos(player);
-        acceleration(player);
-
-        draw_level(level);         // draw the edited map
-        render_screen(player);     // draw the player on top
-
-        refresh();
-    }
-
-    // Clean up
-    free(player);
-    for (int i = 0; i < level->height; i++) free(level->grid[i]);
-    free(level->grid);
-    free(level);
-
     endwin();
     return 0;
 }
+
+//to compile gcc -o map_editor main.c map_editor.c render.c render.h -lm -lncurses
